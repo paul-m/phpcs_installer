4675708cce26a878fbcd50e110c75f9cb205b5e8
Maik Penz <maik@phpkuh.de>
2013-03-16 22:01:47 +0100

    allow custom standard name using extra:phpcs-standard. fixes #2

--- Goatherd/Phpcs/Installer.php
+++ Goatherd/Phpcs/Installer.php
@@ -6,15 +6,23 @@ use Composer\Package\PackageInterface;
 use Composer\Installer\LibraryInstaller;
 
 /**
+ * Install within CodeSniffer.
  *
+ * Name of the package standard defaults to the pretty name.
+ * It may be replaced by the extra:phpcs-standard property of the composer.json.
  */
 class Installer extends LibraryInstaller
 {
     /** {@inheritDoc} */
     public function getInstallPath(PackageInterface $package)
     {
-        // fix package name to denote single directory
-        $name = str_replace(array('/', '\\'), '-', $package->getPrettyName());
+        $extra = $package->getExtra();
+        $name =
+            isset($extra['phpcs-standard'])
+            ? $extra['phpcs-standard']
+            : $package->getPrettyName();
+        // package name must denote a single directoy only
+        $name = str_replace(array('/', '\\'), '-', $name);
         return $this->vendorDir . '/squizlabs/php_codesniffer/CodeSniffer/Standards/' . $name;
     }
 
