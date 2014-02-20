b46c3914780ed5327cfad64480ac8c8e5389a96b
Paul Mitchum <paul@mile23.com>
2014-02-20 12:11:55 -0800

    Working plugin class

--- Goatherd/Phpcs/Plugin.php
+++ Goatherd/Phpcs/Plugin.php
@@ -10,8 +10,7 @@ class Plugin implements PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io)
     {
-      echo('activating.....');
-        //$installer = new Installer($io, $composer);
-        //$composer->getInstallationManager()->addInstaller($installer);
+        $installer = new Installer($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
     }
 }
